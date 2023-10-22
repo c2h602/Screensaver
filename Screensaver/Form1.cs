3a19631c302b09dfe2c3d2ece76f30e3c85cfeb2
c2h602 <cup.cakejr247@gmail.com>
2023-10-22 17:49:31 +0300

    Init

---
+++ Screensaver/Form1.cs
@@ -0,0 +1,91 @@
+namespace Screensaver
+{
+	public partial class Form1 : Form
+	{
+
+		public class Star // класс не обходим для хранения коор-т каждой звезды
+		{
+			public float X { get; set; }
+			public float Y { get; set; }
+			public float Z { get; set; }
+		}
+
+		private Star[] stars = new Star[15000]; // массив объектов
+
+		private Random random = new Random(); // для генерации случайных чисел
+
+		private Graphics graphics; // для отрисовки скринсервера с помощью GDI (- это интерфейс Windows для представления графических объектов
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e) // в таймере отрабатывается движение звезд и их отрисовка
+		{
+			graphics.Clear(Color.Black); 
+
+			foreach (var star in stars) // отрисовка
+			{
+				DrawStar(star);
+				MoveStar(star);
+			}
+
+			pictureBox1.Refresh(); // для того, чтобы отрисовка была на пикчебоксе
+		}
+
+		private void MoveStar(Star star)
+		{
+			star.Z -= 15;
+			if (star.Z < 1)
+			{
+				star.X = random.Next(-pictureBox1.Width, pictureBox1.Width);
+				star.Y = random.Next(-pictureBox1.Height, pictureBox1.Height);
+				star.Z = random.Next(1, pictureBox1.Width);
+			}
+		}
+
+		private void DrawStar(Star star)
+		{
+			float starSize = Map(star.Z, 0, pictureBox1.Width, 10, 0);
+
+			float x = Map(star.X / star.Z, 0, 1, 0, pictureBox1.Width) + pictureBox1.Width / 2;
+
+			float y = Map(star.Y / star.Z, 0, 1, 0, pictureBox1.Height) + pictureBox1.Height / 2;
+
+			graphics.FillEllipse(Brushes.Pink, x, y, starSize, starSize);
+		}
+
+		private float Map(float n, float start1,  float stop1, float start2, float stop2)
+		{
+			// Суть метода - преобразовать коор-ты из одной шкалы измерения в другую
+			return ((n - start1) / (stop1 - start2)) * (stop2 - start2) + start2;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			/* 1 параметр - объект в позиции 10, 
+			 * 2 параметр - исходная шкала от 0 до 100
+			 * 3 параметр - шкала, к которой преобраовываем данный объект*/
+			// var result = Map(10, 0, 100, 0, 1000);
+			
+
+			pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height); // создаем битмап с высотой и шириной пикчербокса
+
+			graphics = Graphics.FromImage(pictureBox1.Image); // выделяем место в оперативной памяти под графикс
+
+			for (int i = 0; i < stars.Length; i++) //перебираем массив старс
+			{
+				stars[i] = new Star() // для каждого элемента выделяем место в оперативной памяти, то есть создаем новую звезду
+				{
+					// Присвоение значений в поля XYZ
+					X = random.Next(-pictureBox1.Width, pictureBox1.Width),
+					Y = random.Next(-pictureBox1.Height, pictureBox1.Height),
+					Z = random.Next(1, pictureBox1.Width)
+				};
+			}
+
+			timer1.Start(); 
+		}
+	}
+}
